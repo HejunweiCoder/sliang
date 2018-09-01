ebee171c1499f76c77a7af8de9d4f8e92ab699b0
horan <hejunweimake@gmail.com>
2018-09-01 14:46:13 +0800

    remove cdn

--- header.inc.php
+++ header.inc.php
@@ -14,15 +14,15 @@
 <div class="header">
 			<h1 class="logotext">西安矢量电子科技有限公司</h1>
 			<div class="logo">
-				<a href="index.html"><img src="http://cdn.sliangpos.com/images/logo.png" alt="logo" ></a>
+				<a href="index.html"><img src="http://sliangpos.com/images/logo.png" alt="logo" ></a>
 			</div>
 			<div class="navigation">
 				 <span class="menu"></span> 
 					<ul class="navig">
-						<li><a href="index.php?nav=1" id="nav1">首页</a></li><span><img src="http://cdn.sliangpos.com/images/clk.png" alt="" /></span>
-						<li><a href="about-us.php?nav=2" id="nav2">公司简介</a></li><span><img src="http://cdn.sliangpos.com/images/clk.png" alt="" /></span>
-						<li><a href="services.php?nav=3" id="nav3">业务中心</a></li><span><img src="http://cdn.sliangpos.com/images/clk.png" alt="" /></span>
-						<li><a href="carrers.php?nav=4" id="nav4">就业信息</a></li><span><img src="http://cdn.sliangpos.com/images/clk.png" alt="" /></span>
+						<li><a href="index.php?nav=1" id="nav1">首页</a></li><span><img src="http://sliangpos.com/images/clk.png" alt="" /></span>
+						<li><a href="about-us.php?nav=2" id="nav2">公司简介</a></li><span><img src="http://sliangpos.com/images/clk.png" alt="" /></span>
+						<li><a href="services.php?nav=3" id="nav3">业务中心</a></li><span><img src="http://sliangpos.com/images/clk.png" alt="" /></span>
+						<li><a href="carrers.php?nav=4" id="nav4">就业信息</a></li><span><img src="http://sliangpos.com/images/clk.png" alt="" /></span>
 						<li><a href="contact.php?nav=5" id="nav5">联系我们</a></li>
 					</ul>
 			</div>
